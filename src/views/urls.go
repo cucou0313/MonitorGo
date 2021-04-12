6d2b29666fb149abe8924c23cc81caab572f3c1e
Cucou <512452702@qq.com>
2021-04-12 22:11:14 +0800

    22点11分

--- src/views/urls.go
+++ src/views/urls.go
@@ -15,6 +15,17 @@ import (
 func InitRouter() *gin.Engine {
 	router := gin.Default()
 
+	router.GET("/test", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"SystemCPU":  []int{8, 9, 7, 10, 8, 20},
+			"SystemMem":  []int{33, 36, 40, 44, 55, 66},
+			"ProcessCPU": []int{1, 2, 1, 0, 0, 1},
+			"ProcessMem": []int{1, 2, 2, 1, 1, 0},
+			"ProcessRss": []int{40, 60, 400, 100, 50, 60},
+			"DateTime":   []int{1, 2, 3, 4, 5, 6},
+		})
+	})
+
 	task := router.Group("/task")
 	{
 		task.GET("/get", controller.GetTaskHandler)
